b0fd41f7b33106ba294979c750de185525995e0a
vishesh <visheshgautam.official@gmail.com>
2024-12-15 21:08:04 +0530

    thoda thik

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client';
 const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
-
-const URL = process.env.NODE_ENV === 'production' ? undefined : `${serverUrl}`;
-export const socket = io(URL,{
+export const socket = io(serverUrl,{
     autoConnect :false
 });
