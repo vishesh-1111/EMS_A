b997de8db038e7b5b56be6bb06650446a064ebff
vishesh <visheshgautam.official@gmail.com>
2025-01-17 14:00:35 +0530

    IDK

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,13 +9,27 @@ authRouter.get(
   passport.authenticate('google', { scope: ['email', 'profile', 'openid'] })
 )
 
-.get( '/google/callback',
-    passport.authenticate( 'google', {
-
-      successRedirect:process.env.LOCAL_ORIGIN|| process.env.VERCEL_ORIGIN,
-      failureRedirect: `${process.env.LOCAL_ORIGIN||process.env.VERCEL_ORIGIN}/login/user`
-    })
-  );
+.get('/google/callback',
+  (req, res, next) => {
+    passport.authenticate('google', (err, user, info) => {
+      if (err) {
+        console.error('Error during authentication:', err);
+        return res.redirect(`${process.env.LOCAL_ORIGIN || process.env.VERCEL_ORIGIN}/login/user`);
+      }
+      if (!user) {
+        console.error('No user found:', info);
+        return res.redirect(`${process.env.LOCAL_ORIGIN || process.env.VERCEL_ORIGIN}/login/user`);
+      }
+      req.login(user, (err) => {
+        if (err) {
+          console.error('Login failed:', err);
+          return res.redirect(`${process.env.LOCAL_ORIGIN || process.env.VERCEL_ORIGIN}/login/user`);
+        }
+        return res.redirect(process.env.LOCAL_ORIGIN || process.env.VERCEL_ORIGIN);
+      });
+    })(req, res, next);
+  }
+);
 
 module.exports = authRouter
 
