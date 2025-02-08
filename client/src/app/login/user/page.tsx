528e71865ad634543e2c6024a5d2954355e7816a
vishesh <visheshgautam.official@gmail.com>
2025-02-08 20:00:31 +0530

    fix 10

--- client/src/app/login/user/page.tsx
+++ client/src/app/login/user/page.tsx
@@ -54,6 +54,9 @@ export default function UserLoginForm() {
     router.push("/");
   };
 
+  const AdminLoginHandler = () => {
+    router.push("/login/admin");
+  };
   return (
 
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-4  shadow-input bg-white dark:bg-black">
@@ -110,6 +113,15 @@ export default function UserLoginForm() {
           onClick={GuestLoginHandler}
           >
           {isLoading ? "Loading..." : "Login as guest"}
+          <BottomGradient />
+        </button>
+
+        <button
+          className={`mt-10 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]`}
+
+          onClick={AdminLoginHandler}
+          >
+          Admin Login
           <BottomGradient />
         </button>
   
